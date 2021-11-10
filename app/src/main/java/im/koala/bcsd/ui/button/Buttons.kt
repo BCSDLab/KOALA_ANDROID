1df72c056296a013ab6106838819cdb8860d0fe1
Seungmin Yang <tmdals099@gmail.com>
2021-11-10 22:57:58 +0900

    Add KoalaButtonColor object
    Contains backgroundColor, contentColor, disabledBackgroundColor, disabledContentColor

--- app/src/main/java/im/koala/bcsd/ui/button/Buttons.kt
+++ app/src/main/java/im/koala/bcsd/ui/button/Buttons.kt
@@ -1,7 +1,6 @@
 package im.koala.bcsd.ui.button
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
@@ -10,41 +9,49 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import im.koala.bcsd.ui.theme.Black
 import im.koala.bcsd.ui.theme.GrayDisabled
 import im.koala.bcsd.ui.theme.KoalaTheme
+
+object KoalaButtonColors {
+
+    val backgroundColor: Color
+        @Composable get() = if (isSystemInDarkTheme()) Black else Black
+
+    val contentColor: Color
+        @Composable get() = if (isSystemInDarkTheme()) White else White
+
+    val disabledBackgroundColor: Color
+        @Composable get() = if (isSystemInDarkTheme()) GrayDisabled else GrayDisabled
+
+    val disabledContentColor: Color
+        @Composable get() = if (isSystemInDarkTheme()) White else White
+}
 
 @Composable
 fun KoalaButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     content: @Composable RowScope.() -> Unit
 ) {
     Button(
         onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        interactionSource = interactionSource,
-        elevation = ButtonDefaults.elevation(
-            defaultElevation = 0.dp,
-            pressedElevation = 8.dp,
-            disabledElevation = 0.dp
-        ),
         shape = MaterialTheme.shapes.medium,
         border = null,
         colors = ButtonDefaults.buttonColors(
-            backgroundColor = MaterialTheme.colors.secondary,
-            contentColor = MaterialTheme.colors.onSecondary,
-            disabledBackgroundColor = GrayDisabled,
-            disabledContentColor = MaterialTheme.colors.onSecondary
+            backgroundColor = KoalaButtonColors.backgroundColor,
+            contentColor = KoalaButtonColors.contentColor,
+            disabledBackgroundColor = KoalaButtonColors.disabledBackgroundColor,
+            disabledContentColor = KoalaButtonColors.disabledContentColor
         ),
-        contentPadding = contentPadding,
         content = content
     )
 }
@@ -55,7 +62,9 @@ fun KoalaButtonPreview() {
     KoalaTheme {
         KoalaButton(
             onClick = {},
-            modifier = Modifier.width(320.dp).height(48.dp),
+            modifier = Modifier
+                .width(320.dp)
+                .height(48.dp),
             enabled = true
         ) {
             Text(text = "Enabled")
@@ -69,7 +78,9 @@ fun KoalaButtonDisabledPreview() {
     KoalaTheme {
         KoalaButton(
             onClick = {},
-            modifier = Modifier.width(320.dp).height(48.dp),
+            modifier = Modifier
+                .width(320.dp)
+                .height(48.dp),
             enabled = false
         ) {
             Text(text = "Disabled")
